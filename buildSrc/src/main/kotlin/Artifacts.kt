655ddc16282b6e6bbf5b9e876752fc433229594e
psuzn <mail@sujanpoudel.me>
2023-12-21 11:36:00 +0545

    fix: code useless cast

--- buildSrc/src/main/kotlin/Artifacts.kt
+++ buildSrc/src/main/kotlin/Artifacts.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.Project
 
 
-fun Project.group(): String = property("GROUP") as String
-fun Project.versionName(): String = property("VERSION_NAME") as String
+fun Project.group() = property("GROUP") as String
+fun Project.versionName() = property("VERSION_NAME") as String
 
 fun Project.sampleVersionCode() = 1
 fun Project.sampleVersionName() = "0.0.1"
